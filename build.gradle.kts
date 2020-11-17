5b8bc3da84280bc1a1ed495493ff4bd6212fd3ad
Torben <torben.schmitz@outlook.de>
2020-11-17 21:12:35 +0100

    Add JUnit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ version = "1.0"
 
 plugins {
     kotlin("jvm") version "1.4.10"
-    maven
 }
 
 kotlin {
@@ -18,6 +17,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor:ktor-network:1.4.1")
     implementation("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks {
@@ -26,5 +28,8 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
     }
 }
